28ee465c3d3f01c57f9e13de29efcea9af8ba6d4
MicaelaDeRosa23 <micaaderosa@gmail.com>
2024-09-29 03:05:37 +0000

    MicaelaDeRosa.RandomCardGenerator

--- src/app.js
+++ src/app.js
@@ -5,7 +5,35 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+window.addEventListener("load", () => {
+  const values = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  const symbols = ["♠", "♣", "♦", "♥"];
+  const colors = ["red", "black"];
+
+  const cardSymbolHeader = document.getElementById("card-symbol-header");
+  const cardValue = document.getElementById("card-value");
+  const cardSymbolFooter = document.getElementById("card-symbol-footer");
+
+  const randomValueIndex = Math.floor(Math.random() * values.length);
+  const randomSymbolIndex = Math.floor(Math.random() * symbols.length);
+  const randomColorIndex = Math.floor(Math.random() * colors.length);
+
+  cardSymbolHeader.innerHTML = `<p style="color:${colors[randomColorIndex]}">${symbols[randomSymbolIndex]}</p>`;
+  cardValue.innerHTML = `<p>${values[randomValueIndex]}</p>`;
+  cardSymbolFooter.innerHTML = `<p style="color:${colors[randomColorIndex]}">${symbols[randomSymbolIndex]}</p>`;
+});
